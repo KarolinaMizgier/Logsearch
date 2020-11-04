35fd7a904e0ed0ccdb5a8574d0fe1ebee2b8b7af
karolinamizgier <karolinamizgier@gmail.com>
2020-11-04 18:32:37 +0100

    Added line number

--- src/main/java/org/karolinaprojects/logsearch/LogSearch.java
+++ src/main/java/org/karolinaprojects/logsearch/LogSearch.java
@@ -1,8 +1,5 @@
 package org.karolinaprojects.logsearch;
 
-import java.io.FileReader;
-import java.io.LineNumberReader;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +8,10 @@ public class LogSearch {
         List<String> listClone = new ArrayList<>();
         for(int i=0; i < list.size(); i++){
             String line = list.get(i);
-        if (line.contains(key)) {
+            if (line.contains(key)) {
                 listClone.add(line + " | line number: " +i);
             }
         }
-
         return listClone;
     }
 }
--- src/main/java/org/karolinaprojects/logsearch/Main.java
+++ src/main/java/org/karolinaprojects/logsearch/Main.java
@@ -1,22 +1,13 @@
 package org.karolinaprojects.logsearch;
 
-import java.io.FileReader;
-import java.io.LineNumberReader;
-import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> Text = FileUtil.read("src/main/resources/Text");
         List<String> result = LogSearch.search(Text,"mailbox");
-        result.forEach(line->
-                System.out.println(line)
-                );
-
-
+        result.forEach(System.out::println);
 
         // zliczanie ilosci wystapien danego wyrazenia w tekscie
-        // wyswietlanie całej linii tekstu z frazą oraz numeru linii
     }
 }
